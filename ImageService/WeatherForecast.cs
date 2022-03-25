8ddef94012686cb10327a5b42bdbaf03ac4c8fe2
Mket <MKet@users.noreply.github.com>
2022-03-25 12:38:34 +0100

    Update .NET version to 6

--- ImageService/WeatherForecast.cs
+++ ImageService/WeatherForecast.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace ImageService
 {
     public class WeatherForecast
@@ -10,6 +8,6 @@ namespace ImageService
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
