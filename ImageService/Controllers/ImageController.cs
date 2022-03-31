cb843b4eadb24f955dfc39fc7622b0c90f91a9ce
Mket <MKet@users.noreply.github.com>
2022-03-31 11:10:22 +0200

    Add azure blob storage dependency

---
+++ ImageService/Controllers/ImageController.cs
@@ -0,0 +1,74 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Net.Http.Headers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ImageService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private readonly BlobServiceClient blobClient;
+
+        public ImageController(BlobServiceClient blobClient)
+        {
+            this.blobClient = blobClient ?? throw new ArgumentNullException(nameof(blobClient));
+        }
+
+        // GET: api/<ImageController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ImageController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ImageController>
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] string username)
+        {
+            var request = HttpContext.Request;
+
+            if (!request.HasFormContentType ||
+                !MediaTypeHeaderValue.TryParse(request.ContentType, out var mediaTypeHeader) ||
+                string.IsNullOrEmpty(mediaTypeHeader.Boundary.Value))
+            {
+                return new UnsupportedMediaTypeResult();
+            }
+
+            var reader = new MultipartReader(mediaTypeHeader.Boundary.Value, request.Body);
+            var section = await reader.ReadNextSectionAsync();
+            return new OkResult();
+
+
+            while (section != null)
+            {
+                var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition,
+                    out var contentDisposition);
+
+
+            }
+        }
+
+        // PUT api/<ImageController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ImageController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
